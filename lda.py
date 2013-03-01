d780fca4f2a12aef4b306136c12b78cd0c1d500b
Fancy Simon <fancysimon@163.com>
2013-03-01 23:38:01 +0800

    mpi: likelihood and so on

--- lda.py
+++ lda.py
@@ -13,18 +13,20 @@ def parse_args():
 	parser = OptionParser()
 
 	parser.add_option("-a", "--alpha", dest="alpha", type="float", help="alpha")
-	parser.add_option("-b", "--beta", dest="beta", type="float", default=0.01, help="beta")
-	parser.add_option("-k", "--num_topics", dest="num_topics", type="int", help="topic numbers")
-	parser.add_option("--train_name", dest="train_name", type="string", default="train.txt",
-						help="file name of taining data")
-	parser.add_option("--model_name", dest="model_name", type="string", default="model.txt",
-						help="file prefix of model")
+	parser.add_option("-b", "--beta", dest="beta", type="float", default=0.01,
+						help="beta")
+	parser.add_option("-k", "--num_topics", dest="num_topics", type="int",
+						help="topic numbers")
+	parser.add_option("--train_name", dest="train_name", type="string",
+						default="train.txt", help="file name of taining data")
+	parser.add_option("--model_name", dest="model_name", type="string",
+						default="model.txt", help="file prefix of model")
 	parser.add_option("--total_iterations", dest="total_iterations", type="int", 
 						default=10, help="total iterations")
-	parser.add_option("--burn_in_iterations", dest="burn_in_iterations", type="int", 
-						default=5, help="burn in iterations")
-	parser.add_option("--compute_likelihood", dest="compute_likelihood", action="store_true",
-						help="compute log likelihood")
+	parser.add_option("--burn_in_iterations", dest="burn_in_iterations",
+						type="int", default=5, help="burn in iterations")
+	parser.add_option("--compute_likelihood", dest="compute_likelihood",
+						action="store_true", help="compute log likelihood")
 
 	(options, args) = parser.parse_args()
 	if not options.num_topics:
@@ -41,7 +43,8 @@ def load_corpus(train_name, num_topics):
 	corpus = []
 	word_id_map = {}
 	for line in train_file:
-		if len(line) == 0 or line[0] == "\n" or line[0] == "\r" or line[0] == "#":
+		if len(line) == 0 or line[0] == "\n" or \
+				line[0] == "\r" or line[0] == "#":
 			continue
 		document = Document()
 		document.load_document(line, word_id_map, num_topics)
@@ -64,7 +67,8 @@ def main():
 		print "Iteration:", i
 		sampler.sample_loop(corpus, model)
 		if options.compute_likelihood:
-			print "    Loglikehood:", sampler.compute_log_likelihood(corpus, model)
+			print "    Loglikehood:", \
+					sampler.compute_log_likelihood(corpus, model)
 		if i >= options.burn_in_iterations:
 			model.accumulate_model()
 	model.average_accumulative_model()
--- mpi_lda.py
+++ mpi_lda.py
@@ -31,18 +31,20 @@ def parse_args():
 	parser = OptionParser()
 
 	parser.add_option("-a", "--alpha", dest="alpha", type="float", help="alpha")
-	parser.add_option("-b", "--beta", dest="beta", type="float", default=0.01, help="beta")
-	parser.add_option("-k", "--num_topics", dest="num_topics", type="int", help="topic numbers")
-	parser.add_option("--train_name", dest="train_name", type="string", default="train.txt",
-						help="file name of taining data")
-	parser.add_option("--model_name", dest="model_name", type="string", default="model.txt",
-						help="file prefix of model")
+	parser.add_option("-b", "--beta", dest="beta", type="float",
+						default=0.01, help="beta")
+	parser.add_option("-k", "--num_topics", dest="num_topics", type="int",
+						help="topic numbers")
+	parser.add_option("--train_name", dest="train_name", type="string",
+						default="train.txt", help="file name of taining data")
+	parser.add_option("--model_name", dest="model_name", type="string",
+						default="model.txt", help="file prefix of model")
 	parser.add_option("--total_iterations", dest="total_iterations", type="int", 
 						default=10, help="total iterations")
-	parser.add_option("--burn_in_iterations", dest="burn_in_iterations", type="int", 
-						default=5, help="burn in iterations")
-	parser.add_option("--compute_likelihood", dest="compute_likelihood", action="store_true",
-						help="compute log likelihood")
+	parser.add_option("--burn_in_iterations", dest="burn_in_iterations",
+						type="int", default=5, help="burn in iterations")
+	parser.add_option("--compute_likelihood", dest="compute_likelihood",
+						action="store_true", help="compute log likelihood")
 
 	(options, args) = parser.parse_args()
 	if not options.num_topics:
@@ -61,14 +63,16 @@ def distributely_load_corpus(train_name, num_topics, myid, pnum):
 	word_set = set()
 	index = 0
 	for line in train_file:
-		if len(line) == 0 or line[0] == "\n" or line[0] == "\r" or line[0] == "#":
+		if len(line) == 0 or line[0] == "\n" or \
+				line[0] == "\r" or line[0] == "#":
 			continue
 		if index % pnum == myid:
 			document = Document()
 			document.load_document_for_distribute(line, num_topics, word_set)
 			corpus.append(document)
 		else:
-			document.load_document_for_distribute(line, num_topics, word_set, True)
+			document.load_document_for_distribute(
+					line, num_topics, word_set, True)
 		index += 1
 	train_file.close()
 
@@ -93,19 +97,29 @@ def main():
 	# 	print d.debug_string()
 
 	sampler = Sampler(options.alpha, options.beta)
+	for i in range(options.total_iterations):
+		print "Iteration:", i
+		model = ParallelModel()
+		model.init_model(len(corpus_local), options.num_topics, len(word_id_map))
+		sampler.init_model_given_corpus(corpus_local, model)
+		model.AllReduceModel(comm)
+		sampler.sample_loop(corpus_local, model)
+		if options.compute_likelihood:
+			loglikelihood_local =
+					sampler.compute_log_likelihood(corpus_local, model)
+			loglikelihood_gobal = 0.0
+			comm.Reduce(loglikelihood_local, loglikelihood_gobal, MPI.SUM, 0)
+			if myid == 0:
+				print "    Loglikehood:", loglikelihood_gobal
+		if i >= options.burn_in_iterations:
+			model.accumulate_model()
 	model = ParallelModel()
 	model.init_model(len(corpus_local), options.num_topics, len(word_id_map))
 	sampler.init_model_given_corpus(corpus_local, model)
-	mode.AllReduceModel(comm)
-	for i in range(options.total_iterations):
-		print "Iteration:", i
-		sampler.sample_loop(corpus_local, model)
-		# if options.compute_likelihood:
-		# 	print "    Loglikehood:", sampler.compute_log_likelihood(corpus_local, model)
-		if i >= options.burn_in_iterations:
-			model.accumulate_model()
-	model.average_accumulative_model()
-	model.save_model(options.model_name, word_id_map)
+	model.AllReduceModel(comm)
+	if myid == 0:
+		model.average_accumulative_model()
+		model.save_model(options.model_name, word_id_map)
 
 if __name__ == "__main__":
 	main()
